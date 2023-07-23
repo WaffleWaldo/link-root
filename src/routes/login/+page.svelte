ddfd9a7097ce45e8280fd8b1c73b7064789555fa
WaffleWaldo <oswaldoortiz0519@gmail.com>
2023-07-23 13:46:09 -0400

    added a user store and a sign out btn

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
-    import { auth } from "$lib/firebase.js"
+    // this section can be its own component
+    import { auth, user } from "$lib/firebase.js"
     import { GoogleAuthProvider, signInWithPopup, signOut } from "firebase/auth";
 
     async function signInWithGoogle() {
@@ -9,6 +10,10 @@
     }
 </script>
 
-<h2>Login</h2>
-
+{#if $user}
+    <h2 class="card-title"> Welcome, {$user.displayName}</h2>
+    <p class="text-center text-success">You are logged in</p>
+    <button class="btn btn-danger" on:click={() => signOut(auth)}>Sign Out</button>
+{:else}
 <button class="btn btn-primary" on:click={signInWithGoogle}>Login in with Google</button>
+{/if}
